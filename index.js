87befee6036e6c2ef8b5be29df0f1af38af6c690
Ivan Constantin Raul <constantinraulivan@gmail.com>
2022-10-04 22:32:40 +0300

    hamburger menu

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let allTasks = [];
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
 function loadTask() {
   fetch("http://localhost:3000/tasks-json")
@@ -8,5 +10,17 @@ function loadTask() {
       console.log("tasks", allTasks);
     });
 }
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links").forEach((links) =>
+  links.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  })
+);
 
 loadTask();
